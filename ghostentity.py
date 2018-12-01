8485ebc67fbdda5e249367fcd755fdb25a4c26f9
sizzziy <sizzziy@yandex.ru>
2018-12-01 19:25:00 +0300

    Added ghost entity

---
+++ ghostentity.py
@@ -0,0 +1,15 @@
+import pygame
+from entity import Entity
+
+class Ghost(Entity):
+    def __init__(self, conditions, rect, imgState = 0):
+        super().__init__([], rect, imgState)
+        self.change_type(0) #Направление пакмана 0-вверх, 1-вправо, 2-вниз, 3-влево, 4-смерть.
+
+    def change_type(self, type):
+        self.type = type
+        self.conditions = conditions[type]
+
+    def move(self, dx, dy):
+        self.rect[0] += dx
+        self.rect[1] += dy
