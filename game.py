01786d23bda7a8b3f285de9572ceabeb066cbb18
Longa_Bonga <ivan.dobryaev@mail.ru>
2018-12-06 20:04:31 +0300

    GenerateFruit4

--- game.py
+++ game.py
@@ -62,15 +62,15 @@ class game():
     def set_exit(self):
         self.immediately_close = True
 
-    # def loop(self):
-    #     self.screen.fill((0, 0, 0))
-    #     for object in self.objects:
-    #         object.draw(self.screen)
-    #     if self.state == STATES["game"]:
-    #         for v in self.graph.coordinates:                     ##Рисует вершины графа для проверки
-    #              pygame.draw.circle(self.screen, (0, 255, 0), v, 1)
+    def loop(self):
+        self.screen.fill((0, 0, 0))
+        for object in self.objects:
+            object.draw(self.screen)
+        #if self.state == STATES["game"]:
+            # for v in self.graph.coordinates:                     ##Рисует вершины графа для проверки
+            #      pygame.draw.circle(self.screen, (0, 255, 0), v, 1)
 
-    #    pygame.display.flip()
+        pygame.display.flip()
 
     def game_logic(self):
         pass
