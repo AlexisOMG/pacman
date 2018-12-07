125590836a31e319f84ab563c6072733e4759a27
Artherus <aarchy333@mail.ru>
2018-12-07 19:46:24 +0000

    seed class created

---
+++ seed.py
@@ -0,0 +1,16 @@
+import pygame
+from entity import Entity
+
+class Seed(Entity):
+    def __init__(self, conditions, rect, imgState = 0):
+        super().__init__([], rect, imgState)
+        self.speed = speed
+        self.IsMoving = True
+        self.allConditions = conditions
+        self.conditions = []
+        self.change_type(0) #0 - семечко не съедено, 1 - семечко съедено
+
+    def change_type(self, type):
+        self.type = type
+        self.conditions = self.allConditions[type]
+
